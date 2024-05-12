c73b557c9c3250e7194798462d844eb8148d2cd6
HimakaraL <himakaral@outlook.com>
2024-05-12 17:06:28 +0530

    storage working

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.taskapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var taskAdapter: TaskAdapter
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        taskAdapter = TaskAdapter(mutableListOf())
+        sharedPreferences = getSharedPreferences("TaskPreferences", Context.MODE_PRIVATE)
+        val savedTasks = sharedPreferences.getString("tasks", null)
+        val tasksListType = object : TypeToken<MutableList<Task>>() {}.type
+        val tasks: MutableList<Task> = Gson().fromJson(savedTasks, tasksListType) ?: mutableListOf()
+
+        taskAdapter = TaskAdapter(tasks)
 
         rVTaskItems.adapter = taskAdapter
         rVTaskItems.layoutManager = LinearLayoutManager(this)
@@ -23,11 +33,18 @@ class MainActivity : AppCompatActivity() {
                 val task = Task(name)
                 taskAdapter.addTask(task)
                 eTTaskName.text.clear()
+                saveTasks()
             }
         }
 
         bDddTask.setOnClickListener {
             taskAdapter.deleteTask()
+            saveTasks()
         }
+    }
+
+    private fun saveTasks() {
+        val tasksJson = Gson().toJson(taskAdapter.tasks)
+        sharedPreferences.edit().putString("tasks", tasksJson).apply()
     }
 }
--- app/src/main/java/com/example/taskapp/TaskAdapter.kt
+++ app/src/main/java/com/example/taskapp/TaskAdapter.kt
@@ -1,22 +1,20 @@
 package com.example.taskapp
 
-import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.todo_list.view.cBox
-import kotlinx.android.synthetic.main.todo_list.view.todoName
+import kotlinx.android.synthetic.main.todo_list.view.*
 
-class TaskAdapter (
-    private val Tasks: MutableList<Task>
-) : RecyclerView.Adapter<TaskAdapter.TaskViewholder>() {
+class TaskAdapter(val tasks: MutableList<Task>) :
+    RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
-    class TaskViewholder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewholder {
-        return TaskViewholder(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        return TaskViewHolder(
             LayoutInflater.from(parent.context).inflate(
                 R.layout.todo_list,
                 parent,
@@ -26,47 +24,39 @@ class TaskAdapter (
     }
 
     fun deleteTask() {
-        Tasks.removeAll { task ->
+        tasks.removeAll { task ->
             task.isDone
         }
         notifyDataSetChanged()
     }
 
-
     fun addTask(task: Task) {
-        Tasks.add(task)
-        notifyItemInserted(Tasks.size - 1)
+        tasks.add(task)
+        notifyItemInserted(tasks.size - 1)
     }
 
-
-
-
-    private fun changeValues(todoName: TextView, isDone: Boolean)
-    {
-        if(isDone){
-            todoName.paintFlags = todoName.paintFlags or STRIKE_THRU_TEXT_FLAG
+    private fun changeValues(todoName: TextView, isDone: Boolean) {
+        if (isDone) {
+            todoName.paintFlags = todoName.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
         } else {
-            todoName.paintFlags = todoName.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+            todoName.paintFlags = todoName.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
         }
     }
 
-    //Assigning task details
-    override fun onBindViewHolder(holder: TaskViewholder, position: Int) {
-        val currentTask = Tasks[position]
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val currentTask = tasks[position]
         holder.itemView.apply {
             todoName.text = currentTask.name
             cBox.isChecked = currentTask.isDone
             changeValues(todoName, currentTask.isDone)
             cBox.setOnCheckedChangeListener { _, isChecked ->
                 changeValues(todoName, isChecked)
-                currentTask.isDone = !currentTask.isDone
+                currentTask.isDone = isChecked
             }
         }
     }
 
-
-    //getSize
     override fun getItemCount(): Int {
-        return Tasks.size
+        return tasks.size
     }
 }
