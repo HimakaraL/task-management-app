563cad54c757d97f44b4c43cf41d1193959938cd
HimakaraL <himakaral@outlook.com>
2024-05-12 16:22:43 +0530

    updated

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.taskapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.bAddTask
-import kotlinx.android.synthetic.main.activity_main.bDddTask
-import kotlinx.android.synthetic.main.activity_main.eTTaskName
-import kotlinx.android.synthetic.main.activity_main.rVTaskItems
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var taskAdapter: TaskAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,17 +17,15 @@ class MainActivity : AppCompatActivity() {
         rVTaskItems.adapter = taskAdapter
         rVTaskItems.layoutManager = LinearLayoutManager(this)
 
-        //AddButton
         bAddTask.setOnClickListener {
             val name = eTTaskName.text.toString()
-            if(name.isNotEmpty()) {
+            if (name.isNotEmpty()) {
                 val task = Task(name)
                 taskAdapter.addTask(task)
                 eTTaskName.text.clear()
             }
         }
 
-        //DeleteButton
         bDddTask.setOnClickListener {
             taskAdapter.deleteTask()
         }
